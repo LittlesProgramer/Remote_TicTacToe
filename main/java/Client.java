a9bd7de6da1b1ac73bfc6c0539d3243abf6df07c
LittlesProgramer <magda.kocon1@op.pl>
2021-02-04 12:54:53 +0100

    update Client class

--- main/java/Client.java
+++ main/java/Client.java
@@ -1,3 +1,26 @@
-public class Client {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
+public class Client {
+    private String ipAddress = null;
+    private int port = 0;
+
+    public Client(String ipAddress, int port) throws IOException {
+        this.ipAddress = ipAddress;
+        this.port = port;
+
+
+    }
+
+    private void connectToServer(String ipAddress,int port) throws IOException {
+        String octets[] = ipAddress.split("\\.");
+        byte bytes[] = new byte[]{(byte)Integer.parseInt(octets[0]),(byte)Integer.parseInt(octets[1]),(byte)Integer.parseInt(octets[2]),(byte)Integer.parseInt(octets[3])};
+        InetAddress inet = InetAddress.getByAddress(bytes);
+        Socket client = new Socket();
+        System.out.println("waiting ...");
+        client.connect(new InetSocketAddress(inet,port));
+        System.out.println("connecting ...");
+    }
 }
