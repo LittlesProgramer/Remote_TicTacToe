1ade34733548c0f6111bb952432b4c395bd7a784
LittlesProgramer <magda.kocon1@op.pl>
2021-02-18 12:51:13 +0100

    update Client_Test class

--- test/java/Client_Test.java
+++ test/java/Client_Test.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 class Client_Test {
     static Client c = null;
@@ -18,9 +24,37 @@ class Client_Test {
         Assertions.assertEquals(false,c.isItWinningMove(false));
     }
 
+    @Test
     void getFigureType_Test(){
         // if YOUR_FIRST_MOVE = (default is false) and whatFigureIs = true then your figur type is Circle
         // else if whatFigureIs = false, then your figur type is Cross
         Assertions.assertEquals("Circle",Client.getFigureType(true));
     }
+
+    @Test
+    @DisplayName("Test add your move to List")
+    void addMoveToList_Test() throws NoSuchFieldException, IllegalAccessException {
+        int sizeMap = 1; //default sizeList is 0
+        int addValueIntoMap = 5; //add value to list
+
+        Class obj = c.getClass();
+        Field field = obj.getDeclaredField("listAllYourMove");
+        field.setAccessible(true);
+        List<Integer> listTest = (List<Integer>)field.get(obj);
+        listTest.add(addValueIntoMap);
+
+        Assertions.assertAll(
+                ()->{Assertions.assertEquals(sizeMap,listTest.size());},
+                ()->{Assertions.assertEquals(addValueIntoMap,(int)listTest.get(0));}
+        );
+
+    }
+
+    @Test
+    @DisplayName("Test add your opponent move to List")
+    void listAllYourOpponentMove_Test(){
+        Class obj = c.getClass();
+
+    }
+
 }
